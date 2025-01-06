5f0d6efe1ddbb59b829d6b902a7a94a41960f6e3
hydrian <ben.tyger@tygerclan.net>
2025-01-06 13:48:37 -0500

    Added nofiiction to building wineprefix #1

--- net.azurewebsites.pathos.sh
+++ net.azurewebsites.pathos.sh
@@ -5,26 +5,40 @@ PATHOS_WINE_DIR="${WINEPREFIX}/drive_c/Games/Pathos"
 PATHOS_WIN_EXE="${PATHOS_WIN_DIR}/PathosGame.exe"
 PATHOS_WINE_EXE="${PATHOS_WINE_DIR}/PathosGame.exe"
 
+function show_message {
+  MSG="${1}"
+  LEVEL="${2:-normal}"
+  echo "### ${MSG} ###"
+  NOTIFY_ID=$(notify-send --urgency="${LEVEL}" -i /app/share/icons/hicolor/scalable/apps/net.azurewebsites.pathos.svg  "${MSG}" -p 2>/dev/null)
+  return 0
+}
+
+### Setting Wine Environment
 function setup_wine {
-  echo "Setting up Wine... This may take a while..."
+  show_message "Setting up Windows (Wine) is being setup. This may take a while."
   echo "Setting-up wine prefix..."
   echo "WINEPREIX: ${WINEPREFIX}"
   WINEDLLOVERRIDES='mscoree=d;mshtml=d' /app/bin/wine64 wineboot
   if [ $? -eq 0 ] ; then 
     echo "Wineboot complete"
   else 
-    echo "Wineboot failed"
+    show_message "Wineboot failed"
     return 3
   fi
   echo "Setting up WineTricks"
   winetricks --unattended 'corefonts' 'dotnet48' 'renderer=gdi'
-  return $?
+  if [ $? -ne 0 ] ; then
+    show_message "Winetricks failed to setup"
+    return 4
+  fi
+  show_message "Windows (Wine) environment setup successfully" low
+  return 0
 }
 #########################
 ### Installing Pathos ###
 #########################
 function install_pathos {
-  echo "Installing Pathos"
+  show_message "Installing Pathos"
   test ! -d "${WINEPREFIX}" && mkdir -p "${WINEPREFIX}"
   WINE_ADVENTURES_DIR="${PATHOS_WINE_DIR}/Adventures"
   FLATPAK_ADVENTURES_DIR='/var/data/adventures'
@@ -34,9 +48,10 @@ function install_pathos {
   echo "Running Pathos installer..."
   wine64 "${INSTALLER_FILE}" "/verysilent" "/dir=${PATHOS_WIN_DIR}" "/LOG" "/DoNotLaunchGame"
   if [ $? -eq 0 ] ; then
-    echo "Pathos installer successful"
+    show_message "Pathos installer successful" low
   else
-    echo "Failed to run Pathos installer successfully"
+    show_message "PathosSetup.exe installer failed"
+    return 2
   fi
 
   test ! -d "${FLATPAK_ADVENTURES_DIR}" && mkdir -p "${FLATPAK_ADVENTURES_DIR}"
@@ -44,6 +59,16 @@ function install_pathos {
     echo "Removing stock Adventures directory" 
     rm -Rf "${WINE_ADVENTURES_DIR}"
   fi
+  FLATPAK_PERSIT_CONFIG_DIRS=(
+    "Settings"
+    "Profiles"
+  )
+  FLATPAK_PERSIST_DATA_DIRS=(
+    "Quests"
+    "Adventures"
+    "Tracks"
+    "Sonics"
+  )
   ln -s "${FLATPAK_ADVENTURES_DIR}" "${WINE_ADVENTURES_DIR}"
   if [ $? -ne 0 ] ; then 
     echo "Failed to make symlink for adventures" 1>&2
