6676616c0e444d710ffdfc286d586772df0a22be
hydrian <ben.tyger@tygerclan.net>
2025-01-05 21:03:57 -0500

    Base import from other repo

---
+++ flatpak-build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S bash
+FLATPAK_BUILDER=$(which flatpak-builder)
+if [ $? -ne 0 ] ; then
+  echo "flatpak-builder is not installed." 1>&2 
+  exit 2
+fi
+
+SCRIPT_FILE=$(realpath "${0}")
+GIT_ROOT_DIR=$(realpath "$(dirname "${SCRIPT_FILE}")") 
+echo "Building Flatpak image..."
+pushd "$GIT_ROOT_DIR" 1>/dev/null
+test ! -d .flatpak && mkdir -p .flatpak
+"${FLATPAK_BUILDER}" .flatpak/build \
+  --ccache \
+  --force-clean \
+  --state-dir=.flatpak/state \
+  --repo=.flatpak/repo  \
+  net.azurewebsites.pathos.yml \
+  --install \
+  --user
+if [ $? -ne 0 ] ; then
+  echo "Failed to build image" 1>&2
+  exit 2
+fi
+popd 1>/dev/null
+echo "Flatpak image built successfully"
+exit 0
